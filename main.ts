876919dfa53d09de37b6e32cf532f68f801877b8
ran.wahle@gong.io <ran.wahle@gong.io>
2025-04-21 19:52:13 +0300

    call then

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log('Hello promise');
+import { MyPromise } from "./my-promise.ts";
+
+const myPromise = new MyPromise<number>((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Resolving...');
+        resolve(0);
+    })
+
+});
+
+myPromise.then(res => console.log(res));
