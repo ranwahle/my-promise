5cd3e1724914265445a06b0b01435ca50bd2fd20
ran.wahle@gong.io <ran.wahle@gong.io>
2025-04-21 22:40:22 +0300

    implement rejection

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 import { MyPromise } from "./my-promise.ts";
 
 const myPromise = new MyPromise<number>((resolve, reject) => {
+    // setTimeout(() => {
+    //     console.log('Resolving...');
+    //     resolve(0);
+    // }, 1000)
     setTimeout(() => {
-        console.log('Resolving...');
-        resolve(0);
-    }, 1000)
+        console.log('rejecting');
+        reject(`That's an error`);
+    })
 
 
 });
 
-console.log({myPromise});
+// console.log({myPromise});
 
 
 myPromise.then(res => {
-    console.log({res, myPromise});
-});
+    console.log({res, myPromise})
+}, err => console.log({err, myPromise}) );
--- my-promise.ts
+++ my-promise.ts
@@ -9,28 +9,40 @@ export class MyPromise<TValue> {
     private state = states.pending;
     private value: TValue;
     private successCallback: (value: TValue) => TValue | void;
+    private error: any;
+    private rejectCallback: (error: any) => any | void;
 
     private resolve(value: TValue) {
         this.state = states.fullFilled;
         this.value = value;
         this.successCallback?.(value);
+  
+    }
 
+    reject(error: any): void {
+        this.state = states.rejected;
+        this.error = error;
+        this.rejectCallback?.(error);
     }
 
     constructor(callback: (resolve: (value: TValue) => void, reject: (err: any) => void) => void) {
-        callback(value => this.resolve(value), () => {
-            console.log('There are no rejections');
-        })
+        callback(value => this.resolve(value), (error) => this.reject(error));
     }
+    
 
-    then(callback: (value: TValue | void) => TValue | void): MyPromise<TValue | void> {
+    then(scueesCallback: (value: TValue | void) => TValue | void,
+rejectCallback: (err: any) => any): MyPromise<TValue | void> {
     
        return new MyPromise((resolve, reject) => {
             if (this.state === states.fullFilled)
             {
-                resolve(callback(this.value) )
-            } else {
-                this.successCallback = value => resolve(callback(value));
+                resolve(scueesCallback(this.value) )
+            } else if (this.state === states.rejected) {
+                reject(rejectCallback?.(this.error));
+            } 
+            else {
+                this.successCallback = value => resolve(scueesCallback(value));
+                this.rejectCallback = error => reject(rejectCallback?.(error));
             }
             
        })
